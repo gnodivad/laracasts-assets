3743e618d45f6dc7c33378621166fc830c6ffc2e
David <dodwei.dev@gmail.com>
2019-03-19 16:38:15 +0800

    Episode 3 - One component per page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -25,16 +25,16 @@
                         <h5 class="uppercase font-bold mb-3">The Brand</h5>
                         <ul class="list-reset">
                             <li class="text-sm leading-loose">
-                                <router-link class="text-black" to="/">Logo</router-link>
+                                <router-link class="text-black" to="/" exact>Logo</router-link>
                             </li>
                             <li class="text-sm leading-loose">
-                                <router-link class="text-black" to="/about">Logo Symbol</router-link>
+                                <router-link class="text-black" to="/logo-symbol">Logo Symbol</router-link>
                             </li>
                             <li class="text-sm leading-loose">
-                                <router-link class="text-black" to="/about">Colors</router-link>
+                                <router-link class="text-black" to="/colors">Colors</router-link>
                             </li>
                             <li class="text-sm leading-loose">
-                                <router-link class="text-black" to="/about">Typography</router-link>
+                                <router-link class="text-black" to="/typography">Typography</router-link>
                             </li>
                         </ul>
                     </section>
@@ -43,16 +43,16 @@
                         <h5 class="uppercase font-bold mb-3">Doodles</h5>
                         <ul class="list-reset">
                             <li class="text-sm leading-loose">
-                                <router-link class="text-black" to="/">Mascot</router-link>
+                                <router-link class="text-black" to="/mascot">Mascot</router-link>
                             </li>
                             <li class="text-sm leading-loose">
-                                <router-link class="text-black" to="/about">Illustrations</router-link>
+                                <router-link class="text-black" to="/illustrations">Illustrations</router-link>
                             </li>
                             <li class="text-sm leading-loose">
-                                <router-link class="text-black" to="/about">Loaders and Animations</router-link>
+                                <router-link class="text-black" to="/loaders-and-animations">Loaders and Animations</router-link>
                             </li>
                             <li class="text-sm leading-loose">
-                                <router-link class="text-black" to="/about">Wallpapers</router-link>
+                                <router-link class="text-black" to="/wallpapers">Wallpapers</router-link>
                             </li>
                         </ul>
                     </section>
